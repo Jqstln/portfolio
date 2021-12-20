b3c23966750b2ec85b398080cd801fa8ae7536d5
Justinnn <63072677+Jqstln@users.noreply.github.com>
2021-12-20 14:15:15 +0100

    Link change of hastebin

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/hastebin", (req, res) => { res.redirect("https://discord.com/oauth2/authorize?client_id=786679486749999214&permissions=3207851399&scope=bot%20applications.commands")});
+app.get("/hastebin", (req, res) => { res.redirect("https://bin.justinnn.dev")});
 app.get("/mcbuild-announcer", (req, res) => { res.redirect("https://discord.gg/GWdnwwYMjZ")});
 
 app.listen(config.port, () => console.log(`The server is now running on port ${config.port}`));
