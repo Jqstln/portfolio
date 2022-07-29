7b6f2564cedb290350aac104f698c8b6afeb5de7
Justinnn <63072677+Jqstln@users.noreply.github.com>
2022-07-29 13:31:33 +0200

    dev

--- public/js/script.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-  Particles.init({
-    selector: '.background'
-  });
-};
---
+++ public/js/typewriter.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".typewriter-text", {
+    strings: ["streamer", "web developer", "cat lover", "dog lover"],
+    typeSpeed: 130,
+    backSpeed: 150,
+    loop: true
+})
