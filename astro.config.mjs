c0a66a2fc3641887d12ca5ef9ec0d5ebd0f7ac92
Justinnn <63072677+Jqstln@users.noreply.github.com>
2023-03-03 15:30:31 +0100

    Remove cloudflare import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
+  output: 'static',
   integrations: [
     tailwind(), 
     compress({
