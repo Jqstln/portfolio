263e4ab2dc931324d1a96f41b8fa89c8eaf97652
Justinnn <hello@justinnn.dev>
2024-03-13 14:10:01 +0100

    updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>My new cool app</title>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react';
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Github, Twitter } from "lucide-react";
@@ -11,25 +12,26 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-12 md:pt-48">
       <div className="header text-center text-slate-400 mx-auto">
-        <h1 className="font-bold text-7xl bg-gradient-to-r from-cyan-500 to-cyan-900 bg-clip-text text-transparent md:whitespace-nowrap">Hey! <br className="md:hidden" /> I'm Justin</h1>
+        <h1 className="font-bold text-7xl bg-gradient-to-r from-cyan-500 to-cyan-200 bg-clip-text text-transparent md:whitespace-nowrap">Hey! <br className="md:hidden" /> I'm Justin</h1>
         <h2 className="text-2xl text-center font-bold">Full Stack Web Developer</h2>
-        <div className="about pt-12 max-w-md mx-auto">
-          <h3 className="text-2xl text-center">A developer based in The Netherlands. Makes stuff for a living, crazy huh? 🎉</h3>
+        <div className="about pt-12 max-w-md mx-auto relative">
+          <h3 className="text-2xl text-center">A developer based in <span className="text-2xl rounded-lg underline-offset-4 underline"><a href='https://en.wikipedia.org/wiki/Rotterdam'>The Netherlands</a></span>. Makes stuff for a living, crazy huh? 🎉</h3>
         </div>
       </div>
-      <div className="pb-64 flex gap-4">
-        <a className="bg-emerald-900 rounded-full w-40 h-14 hover:bg-emerald-500 duration-300 transition text-white text-center font-semibold flex justify-center items-center" href="/projects">
+      <div className="pb-64 flex gap-4 md:pt-12 sm:pt-12">
+        <a className="bg-emerald-900 rounded-full w-40 h-14 hover:bg-emerald-500 duration-300 transition text-white text-center font-semibold flex justify-center items-center px-4 py-2" href="/projects">
           <span className="text-xl">Projects</span>
         </a>
-        <a className="bg-emerald-900 rounded-full w-40 h-14 hover:bg-emerald-500 duration-300 transition text-white text-center font-semibold flex justify-center items-center" href="/contact">
+
+        <a className="bg-emerald-900 rounded-full w-40 h-14 hover:bg-emerald-500 duration-300 transition text-white text-center font-semibold flex justify-center items-center px-4 py-2" href="/contact">
           <span className="text-xl">Contact Me</span>
         </a>
       </div>
       <div className="socials flex gap-4">
-        <a href="https://github.com/Jqstln" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 duration-300 transition">
+        <a href="https://github.com/Jqstln" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-200 duration-300 transition">
           <Github size={48} />
         </a>
-        <a href="https://x.com/Jqstln" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 duration-300 transition">
+        <a href="https://x.com/Jqstln" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-200 duration-300 transition">
           <Twitter size={48} />
         </a>
       </div>
