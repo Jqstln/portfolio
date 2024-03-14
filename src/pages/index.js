3cb06840d32e9fc84e6d94d33bcab4cf01ac171e
Justinnn <hello@justinnn.dev>
2024-03-14 14:26:23 +0100

    added fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,12 @@ export default function Home() {
   };
 
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-between pt-12 md:pt-48">
+    <main className="relative flex min-h-screen flex-col items-center justify-between pt-12 md:pt-48 font-sans">
       <div className="header text-center text-slate-400 mx-auto">
         <h1 className="font-bold text-7xl bg-gradient-to-r from-cyan-500 to-cyan-200 bg-clip-text text-transparent md:whitespace-nowrap">Hey! <br className="md:hidden" /> I&apos;m Justin</h1>
         <h2 className="text-2xl text-center font-bold">Full Stack Web Developer</h2>
         <div className="about pt-12 max-w-md mx-auto relative">
-          <h3 className="text-2xl text-center">A developer based in <span className="text-2xl rounded-lg underline-offset-4 underline"><a href='https://en.wikipedia.org/wiki/Rotterdam' className='cursor-pointer hover:text-blue-300 duration-300 transition'>The Netherlands</a></span>. Makes stuff for a living, crazy huh? <span role="img" aria-label="party emoji" onClick={confetti} onMouseEnter={confetti} className='cursor-pointer'>🎉</span></h3>
+          <h3 className="text-2xl text-center">A developer based in <span className="text-2xl rounded-lg underline-offset-4 underline"><a href='https://en.wikipedia.org/wiki/Rotterdam' className='cursor-pointer hover:text-blue-300 duration-300 transition'>The Netherlands</a></span>. Develops stuff, pretty wild, right? <span role="img" aria-label="party emoji" onClick={confetti} onMouseEnter={confetti} className='cursor-pointer'>🎉</span></h3>
         </div>
       </div>
       <canvas id="tada" className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></canvas>
