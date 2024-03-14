dcbf0e9cf5ebf1d9d2707b2c2d6ed294cb9fa4e0
Justinnn <hello@justinnn.dev>
2024-03-14 12:29:46 +0100

    nog korter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import Confetti from 'confetti-js'; // Import confetti-js library
-import Image from "next/image";
+import Confetti from 'confetti-js';
 import { Inter } from "next/font/google";
 import { Github, Twitter } from "lucide-react";
 
@@ -10,10 +8,10 @@ export default function Home() {
   const currentYear = new Date().getFullYear();
 
   const confetti = () => {
-    const confettiSettings = { target: 'my-canvas' };
-    const confetti = new Confetti(confettiSettings);
+    const settings = { target: 'tada' };
+    const confetti = new Confetti(settings);
     confetti.render();
-    setTimeout(() => confetti.clear(), 1000);
+    setTimeout(() => confetti.clear(), 700);
     const audio = new Audio('/confetti.mp3');
     audio.play();
   };
@@ -24,20 +22,19 @@ export default function Home() {
         <h1 className="font-bold text-7xl bg-gradient-to-r from-cyan-500 to-cyan-200 bg-clip-text text-transparent md:whitespace-nowrap">Hey! <br className="md:hidden" /> I&apos;m Justin</h1>
         <h2 className="text-2xl text-center font-bold">Full Stack Web Developer</h2>
         <div className="about pt-12 max-w-md mx-auto relative">
-          <h3 className="text-2xl text-center">A developer based in <span className="text-2xl rounded-lg underline-offset-4 underline"><a href='https://en.wikipedia.org/wiki/Rotterdam'>The Netherlands</a></span>. Makes stuff for a living, crazy huh? <span role="img" aria-label="party emoji" onClick={confetti} onMouseEnter={confetti} className='cursor-pointer'>🎉</span></h3>
+          <h3 className="text-2xl text-center">A developer based in <span className="text-2xl rounded-lg underline-offset-4 underline"><a href='https://en.wikipedia.org/wiki/Rotterdam' className='cursor-pointer hover:text-blue-300 duration-300 transition'>The Netherlands</a></span>. Makes stuff for a living, crazy huh? <span role="img" aria-label="party emoji" onClick={confetti} onMouseEnter={confetti} className='cursor-pointer'>🎉</span></h3>
         </div>
       </div>
-      <canvas id="my-canvas" className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></canvas>
-      <div className="pb-8 md:pb-24 flex gap-4 md:pt-12 sm:pt-12"> {/* Use smaller padding on smaller screens */}
+      <canvas id="tada" className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></canvas>
+      <div className="pb-4 md:pb-48 flex gap-4 md:pt-12 sm:pt-12">
         <a className="bg-emerald-900 rounded-full w-40 h-14 hover:bg-emerald-500 duration-200 transition text-white text-center font-semibold flex justify-center items-center px-4 py-2" href="/projects">
           <span className="text-xl">Projects</span>
         </a>
-
         <a className="bg-emerald-900 rounded-full w-40 h-14 hover:bg-emerald-500 duration-200 transition text-white text-center font-semibold flex justify-center items-center px-4 py-2" href="/contact">
           <span className="text-xl">Contact Me</span>
         </a>
       </div>
-      <div className="socials flex gap-4 pt-4"> {/* Add pt-4 class to add top padding */}
+      <div className="socials flex gap-4 pt-4">
         <a href="https://github.com/Jqstln" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-200 duration-300 transition">
           <Github size={48} />
         </a>
