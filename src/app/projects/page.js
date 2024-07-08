bda7c561814ef19050022754176d7b6b058cbba7
Justinnn <hello@justinnn.dev>
2024-07-08 19:48:09 +0200

    v0.1.3

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,44 +9,99 @@ export default function Projects() {
         <title>Projects - Justin's Portfolio</title>
       </Head>
       <div className="mb-10 text-center">
-        <h1 className="mb-6 text-6xl font-bold">Projects</h1>
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <Link href="/projects/oasemc" className="group relative overflow-hidden rounded-lg">
-            <Image
-              src={`https://cdn.justinnn.dev/web/oasemc.png`}
-              width={384}
-              height={384}
-              alt="OaseMC"
-              className="transform transition-transform duration-300 group-hover:scale-110 group-hover:blur-sm"
-            />
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        <h1 className="mb-6 text-6xl font-bold">My projects</h1>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <div className="group relative overflow-hidden rounded-lg">
+            <div className="relative h-80 w-80">
+              <Image
+                src={`https://cdn.justinnn.dev/web/oasemc.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="OaseMC"
+                className="transform transition-transform duration-300 group-hover:scale-110 group-hover:blur-sm"
+              />
+            </div>
+            <div className="absolute inset-0 flex w-full flex-col items-center justify-center space-y-4 bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               <span className="text-xl font-bold text-white">OaseMC</span>
+              <span className="w-full max-w-60 text-center text-lg text-white">
+                OaseMC, a Dutch Minecraft server with many game modes, but the primary focus is on
+                survival.
+              </span>
+              <Link href="https://oasemc.nl/links">
+                <button className="rounded-lg bg-sky-400 px-6 py-3 font-bold text-white transition duration-300 hover:bg-sky-500">
+                  Website
+                </button>
+              </Link>
             </div>
-          </Link>
-          <Link href="projects/aloramc" className="group relative overflow-hidden rounded-lg">
-            <Image
-              src={`https://cdn.justinnn.dev/web/aloramc.png`}
-              width={384}
-              height={384}
-              alt="AloraMC"
-              className="transform transition-transform duration-300 group-hover:scale-110 group-hover:blur-sm"
-            />
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          </div>
+          <div className="group relative overflow-hidden rounded-lg">
+            <div className="relative h-80 w-80">
+              <Image
+                src={`https://cdn.justinnn.dev/web/aloramc.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="AloraMC"
+                className="transform transition-transform duration-300 group-hover:scale-110 group-hover:blur-sm"
+              />
+            </div>
+            <div className="absolute inset-0 flex w-full flex-col items-center justify-center space-y-4 bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               <span className="text-xl font-bold text-white">AloraMC</span>
+              <span className="w-full max-w-60 text-center text-lg text-white">
+                AloraMC, an English Minecraft server that focuses on good game modes and currently
+                primarily on CaveBlock.
+              </span>
+              <Link href="https://aloramc.com/">
+                <button className="rounded-lg bg-sky-400 px-6 py-3 font-bold text-white transition duration-300 hover:bg-sky-500">
+                  Website
+                </button>
+              </Link>
             </div>
-          </Link>
-          <Link href="/projects/arc8" className="group relative overflow-hidden rounded-lg">
-            <Image
-              src={`https://cdn.justinnn.dev/web/arc8.png`}
-              width={384}
-              height={384}
-              alt="ARC8"
-              className="transform transition-transform duration-300 group-hover:scale-110 group-hover:blur-sm"
-            />
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          </div>
+          <div className="group relative overflow-hidden rounded-lg">
+            <div className="relative h-80 w-80">
+              <Image
+                src={`https://cdn.justinnn.dev/web/arc8.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="ARC8"
+                className="transform transition-transform duration-300 group-hover:scale-110 group-hover:blur-sm"
+              />
+            </div>
+            <div className="absolute inset-0 flex w-full flex-col items-center justify-center space-y-4 bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               <span className="text-xl font-bold text-white">ARC8</span>
+              <span className="w-full max-w-60 text-center text-lg text-white">
+                ARC8, an discord bot that has a few games, like counting to 100 (without losing) and dice rolling and a lot more!
+              </span>
+              <Link href="https://arc8.fun/">
+                <button className="rounded-lg bg-sky-400 px-6 py-3 font-bold text-white transition duration-300 hover:bg-sky-500">
+                  Website
+                </button>
+              </Link>
             </div>
-          </Link>
+          </div>
+          <div className="group relative overflow-hidden rounded-lg">
+            <div className="relative h-80 w-80">
+              <Image
+                src={`https://cdn.justinnn.dev/web/newproject.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="New Project"
+                className="transform transition-transform duration-300 group-hover:scale-110 group-hover:blur-sm"
+              />
+            </div>
+            <div className="absolute inset-0 flex w-full flex-col items-center justify-center space-y-4 bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              <span className="text-xl font-bold text-white">OaseMC</span>
+              <span className="w-full max-w-60 text-center text-lg text-white">
+                OaseMC, a Dutch Minecraft server with many game modes, but the primary focus is on
+                survival.
+              </span>
+              <Link href="https://google.com/search?q=oasemc">
+                <button className="rounded-lg bg-sky-400 px-6 py-3 font-bold text-white transition duration-300 hover:bg-sky-500">
+                  Website
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
         <div className="mt-10">
           <Link href="/">
