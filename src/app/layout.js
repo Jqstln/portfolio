f4219650b9b8a67d2974a4db3f97eb1afda24d0f
Justinnn <hello@justinnn.dev>
2025-02-15 12:59:49 +0100

    feat: switch to next metadata's api

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { Karla } from 'next/font/google';
 import './globals.css';
 
@@ -6,29 +5,26 @@ const karla = Karla({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Justinnn',
-  description: "Justinnn's Portfolio",
+  description: "A crazy person who likes making things for a living 🎉",
+  keywords: 'justinnn, justin, justinnn.dev, jqstln',
+  authors: [{ name: 'Justinnn' }],
+  robots: 'index, follow',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon/favicon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/favicon/apple-touch-icon.png',
+    shortcut: '/favicon/favicon.ico',
+  },
+  manifest: '/favicon/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
-        <meta name="apple-mobile-web-app-title" content="Justinnn" />
-        <meta name="keywords" content="justinnn,justin,justinnn.dev,jqstln" />
-        <meta name="robots" content="index, follow" />
-        <meta name="author" content="Justinnn" />
-        <title>{metadata.title}</title>
-        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-      </Head>
       <body
-        className={`flex min-h-screen items-center justify-center bg-gradient-to-tr from-gray-900 to-neutral-900 text-white ${karla.className}`}
+        className={`flex min-h-screen items-center justify-center bg-linear-to-tr from-gray-900 to-neutral-900 text-white ${karla.className}`}
       >
         {children}
       </body>
